4260f583a71c13f08c241a4826f54db7451b1e62
milhamramadhan <ramadhanmilham354@gmail.com>
2022-07-07 17:28:54 +0700

    template

---
+++ server.js
@@ -0,0 +1,14 @@
+const Express = require('express');
+const app = Express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
+
