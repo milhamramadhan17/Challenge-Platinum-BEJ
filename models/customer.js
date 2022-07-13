2aff149a2265201b03c61c2611d8c963d629f881
Rais Adi Akbar <101171335+raisadiakbar@users.noreply.github.com>
2022-07-13 10:20:00 +0900

    Update customer.js

--- models/customer.js
+++ models/customer.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-       Customer.belongsTo(models.user, {
+       customer.belongsTo(models.user, {
         foreignKey: 'user_id',
         as: 'user'
       });
