19b321d95bf463053eeab173a0c16a139e2c6117
SistaPanjaitan <sistapanjaitan@gmail.com>
2022-07-19 19:04:56 +0700

    Item router

--- src/route/Items.js
+++ src/route/Items.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const controller = require('../controller/Items');
 const { authentication, authorization } = require('../../middleware/auth');
 
-router.post('/items', authentication, authorization.seller, controller.addItem);
+router.use(authentication)
+
+router.post('/items', authorization.Sellers, controller.addItem);
 router.get('/items', controller.getAll);
 router.get('/items/:id', controller.getByID);
 router.put('/items/:id', controller.updateItems);
-router.delete('/items/:id', authentication, authorization.seller, controller.deleteItem);
+router.delete('/items/:id', authorization.Sellers, controller.deleteItem);
 
 module.exports = router; 
