04104d81592d6349592562d2056d36ca72c6ccee
SistaPanjaitan <sistapanjaitan@gmail.com>
2022-07-19 12:16:44 +0700

    Auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -34,7 +34,23 @@ module.exports = {
               message: 'Token invalid'
             })
           }
+
+          if (!req.headers.authorization) {
+            return res.status(401).json({
+              status: 401,
+              message: 'Unauthorized. Try log in as seller.'
+            });
+          }
       
+          try {
+            req.Sellers = decode(req.headers.authorization);
+
+          } catch (err) {
+            return res.status(401).json({
+              status: 400,
+              message: 'Token invalid'
+            });
+          }
 
         next();
 },
