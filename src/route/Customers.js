8c62b8abe6d9f0ab804c0c2e3882de6044c5bbfd
Rais Adi Akbar <101171335+raisadiakbar@users.noreply.github.com>
2022-07-19 13:47:51 +0900

    Update Customers.js

--- src/route/Customers.js
+++ src/route/Customers.js
@@ -5,7 +5,8 @@ router.post('/api/customer/register', controller.register);
 router.post('/api/customer/login', controller.login);
 
 
-router.get('/customers', controller.getAll);
+
+router.get('/customers', authentication, authorization.customer, controller.getAll);
 
 
 module.exports = router;
