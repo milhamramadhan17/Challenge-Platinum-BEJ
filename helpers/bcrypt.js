1b3c3e4b9d2081820b1ade9f72a334127df4442e
Rais Adi Akbar <101171335+raisadiakbar@users.noreply.github.com>
2022-07-19 21:23:05 +0900

    Update bcrypt.js

--- helpers/bcrypt.js
+++ helpers/bcrypt.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(8);
 
 const hash = (text) => {
-<<<<<<< HEAD
+
   return bcrypt.hashSync(text, salt);
 }
 
@@ -13,16 +13,4 @@ const validateText = (text, hash) => {
 module.exports = {
   hash,
   validateText
-=======
-    return bcrypt.hashSync(text, salt);
-}
-
-const validateText = (text, hash) => {
-    return bcrypt.compareSync(text, hash);
-}
-
-module.exports = {
-    hash,
-    validateText
->>>>>>> 20275fa53674cb208333da73a77871443b0e5b39
 }
