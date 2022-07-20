76f5f967df13ef4011e98ff3cdf2e357f9a6a1e5
raisadiakbar <muhrais791@gmail.com>
2022-07-20 15:17:09 +0900

    Merge branch 'main' of https://github.com/milhamramadhan17/Challenge-Platinum-BEJ

---
+++ migrations/20220720061024-create-customers.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Customers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        type: Sequelize.UUID
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Customers');
+  }
+};
