47ef2031f6a0837fbaf036b089868ea7d97ed469
raisadiakbar <muhrais791@gmail.com>
2022-07-23 20:10:58 +0900

    Customer photo be added

--- models/Customers.js
+++ models/Customers.js
@@ -17,7 +17,8 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    role: DataTypes.INTEGER
+    role: DataTypes.INTEGER,
+    photo: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'Customers',
