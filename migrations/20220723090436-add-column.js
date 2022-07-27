b1c9b8fc25ae6f87f312b5f8a02ae1d11545cbf7
raisadiakbar <muhrais791@gmail.com>
2022-07-27 15:21:28 +0900

    photo

--- migrations/20220723090436-add-column.js
+++ migrations/20220723090436-add-column.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
 
-     await queryInterface.addColumn('Customers', 'Photo', { type: Sequelize.STRING });
+     await queryInterface.addColumn('Customers', 'photo', { type: Sequelize.STRING });
 
   },
 
