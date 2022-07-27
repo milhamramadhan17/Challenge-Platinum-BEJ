8c1144d536d0e417cc41f742c2e18e9c17d50bba
Rais Adi Akbar <101171335+raisadiakbar@users.noreply.github.com>
2022-07-27 15:58:53 +0900

    Update Customers.js

--- models/Customers.js
+++ models/Customers.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     role: DataTypes.INTEGER,
-    Photo: DataTypes.STRING,
+    photo: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'Customers',
