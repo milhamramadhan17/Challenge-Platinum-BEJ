e056471a4b194afbff3a97664db30be1a80cbe33
milhamramadhan <ramadhanmilham354@gmail.com>
2022-08-21 17:05:02 +0700

    a

--- models/index.js
+++ models/index.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '../config/db')[env];
+const env = process.env.NODE_ENV || 'production';
+const config = require(__dirname + '../config/config.json')[env];
 const db = {};
 
 let sequelize;
